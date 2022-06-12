09eb0f5dd8149eec94b8c1e2fc7e41ae63464d8a
@sandeepkumar9 <sv419459@gmail.com>
2022-06-12 09:09:28 +0530

    hello

---
+++ app/Controllers/Login.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function index()
+    {
+     
+     
+     
+        echo view('layout/header');
+        echo view('login_view');
+
+
+
+
+
+    }
+}
---
+++ public/layout/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Register Form</title>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">	
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+<link rel="stylesheet" href="<?=base_url('assets2')?>/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?=base_url('assets2')?>/css/global.css">	
+
+<script src="<?=base_url('assets2')?>/js/jquery.min.js"></script>	
+<script src="<?=base_url('assets2')?>/js/popper.min.js"></script>
+<script src="<?=base_url('assets2')?>/js/bootstrap.min.js"></script>	
+<script src="<?=base_url('assets2')?>/js/global.js"></script>	
+</head>
+
+<body>
